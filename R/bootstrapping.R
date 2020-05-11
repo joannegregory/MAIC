a94dc0ca0d8fe92b4c5466bbd0403ccbdc21ec4f
joannegregory <58074283+joannegregory@users.noreply.github.com>
2020-05-11 12:04:54 +0100

    JG model in bootstrapping

--- R/bootstrapping.R
+++ R/bootstrapping.R
@@ -3,25 +3,22 @@
 #'
 #' Bootstrapping of the hazard ratio to capture the uncertainty in the
 #' estimation of MAIC propensity weights using the following general process:
-#' \enumerate{
-#'    \item Sample patients with replacement from the intervention study
-#'    \item Estimate propensity weights for the current sample of patients
-#'    \item Calculate a weighted hazard ratio
-#'    \item Repeat the steps above N times. Approx. 1000 simulations are usually
-#'    sufficient
-#' }
 #' bootstrapped samples
 #'
-#' @param analysis_data  A data frame containing data with weights (derived from
-#'   \code{\link{analysis_dataset}}).
-#' @param n_sim  Number of simulations. Default = 1000.
+#' @param intervention_data  A data frame containing data with weights (derived from
+#'   \code{\link{est_weights}}).
+#' @param comparator_data
+#' @param matching
+#' @param i
+#' @param model A model formula in the form 'Surv(Time, Event==1) ~ ARM'.
+#'   Variable names need to match the corresponding columns in intervention_data
 #'
-#' @return A data frame of n_sim bootstrapped HR values.
+#' @return A HR value.
 #'
-#' @seealso \code{\link{analysis_dataset}}, \code{\link{estimate_weights}}, \code{\link{HR_summary}}
+#' @seealso \code{\link{analysis_dataset}}, \code{\link{estimate_weights}},
 #' @export
 
-bootstrap_HR <- function(intervention_data, comparator_data, matching, i, Time=Time, Event=Event){
+bootstrap_HR <- function(intervention_data, comparator_data, matching, i, model){
 
   # Samples the data
   bootstrap_data <- intervention_data[i,]
@@ -30,7 +27,7 @@ bootstrap_HR <- function(intervention_data, comparator_data, matching, i, Time=T
   perform_wt <- estimate_weights(intervention_data=bootstrap_data, matching_vars=matching, comparator_data=comparator_data)
 
   # survival data stat
-  cox_model <- coxph(Surv(Time, Event==1) ~ ARM, data = perform_wt$analysis_data, weights = wt)
+  cox_model <- coxph(model, data = perform_wt$analysis_data, weights = wt)
   HR <- exp(cox_model$coefficients)
 }
 
@@ -55,14 +52,13 @@ bootstrap_HR <- function(intervention_data, comparator_data, matching, i, Time=T
 #' @param comparator_data
 #' @param matching
 #' @param i
-#' @param endpoint The variable name for the binary endpoint.
+#' @param model A model formula in the form 'endpoint ~ treatment_var'.
 #'   Variable names need to match the corresponding columns in intervention_data
-#'
 #' @return A data frame of n_sim bootstrapped OR values.
 #'
 #' @seealso \code{\link{analysis_dataset}}, \code{\link{estimate_weights}}, \code{\link{OR_summary}}
 #' @export
-bootstrap_OR <- function(intervention_data, comparator_data, matching, i, endpoint){
+bootstrap_OR <- function(intervention_data, comparator_data, matching, i, model){
 
   # Samples the data
   bootstrap_data <- intervention_data[i,]
@@ -71,10 +67,9 @@ bootstrap_OR <- function(intervention_data, comparator_data, matching, i, endpoi
   perform_wt <- estimate_weights(intervention_data=bootstrap_data, matching_vars=matching,  comparator_data=comparator_data)
 
   # Perform logistic regression and extract the OR estimate
-  logistic.regr <- suppressWarnings(glm(formula = endpoint~ARM, family=binomial(link="logit"), data = perform_wt$analysis_data, weight = wt))
+  logistic.regr <- suppressWarnings(glm(formula = model, family=binomial(link="logit"), data = perform_wt$analysis_data, weight = wt))
   OR <- exp(as.numeric(coef(logistic.regr)[2]))
 }
-
 
 
 #' Bootstrapping for MAIC propensity weighted relative risk HELP PAGE NEEDS UPDATING
@@ -95,14 +90,14 @@ bootstrap_OR <- function(intervention_data, comparator_data, matching, i, endpoi
 #' @param comparator_data
 #' @param matching
 #' @param i
-#' @param endpoint The variable name for the binary endpoint.
+#' @param model A model formula in the form 'endpoint ~ treatment_var'.
 #'   Variable names need to match the corresponding columns in intervention_data
 #'
 #' @return A data frame of n_sim bootstrapped OR values.
 #'
 #' @seealso \code{\link{analysis_dataset}}, \code{\link{estimate_weights}}, \code{\link{OR_summary}}
 #' @export
-bootstrap_RR <- function(intervention_data, comparator_data, matching, i, endpoint){
+bootstrap_RR <- function(intervention_data, comparator_data, matching, i, model){
 
   # Samples the data
   bootstrap_data <- intervention_data[i,]
@@ -111,7 +106,7 @@ bootstrap_RR <- function(intervention_data, comparator_data, matching, i, endpoi
   perform_wt <- estimate_weights(intervention_data=bootstrap_data, matching_vars=matching,  comparator_data=comparator_data)
 
   # Perform logistic regression and extract the RR estimate
-  poisson_regr <- suppressWarnings(glm(formula = endpoint~ARM, family=poisson(link="log"), data = perform_wt$analysis_data, weight = wt))
+  poisson_regr <- suppressWarnings(glm(formula = model, family=poisson(link="log"), data = perform_wt$analysis_data, weight = wt))
   RR <- exp(as.numeric(coef(poisson_regr)[2]))
 }
 
